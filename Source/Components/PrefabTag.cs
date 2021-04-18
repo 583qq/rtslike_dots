613cddd7cf53af1ff664ee2bd803f7c4e6a0ba7e
Vitaliy <ggnet.mtd@gmail.com>
2021-04-18 15:41:42 +0700

    dir changes

--- Source/Components/PrefabTag.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Mathematics;
-
-
-public struct PrefabTag : IComponentData {}
-public struct SelectableTag : IComponentData {}
